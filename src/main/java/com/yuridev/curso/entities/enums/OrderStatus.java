3c35aaa54652faed5a8cb2fa0f6b643944d35dac
Yuri Miyazaki <yuritakeo@ucl.br>
2020-10-28 10:17:55 -0300

    OrderStatus enum

---
+++ src/main/java/com/yuridev/curso/entities/enums/OrderStatus.java
@@ -0,0 +1,30 @@
+package com.yuridev.curso.entities.enums;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT(1),
+	PAID(2),
+	SHIPPED(3),
+	DELIVERED(4),
+	CANCELED(5);
+	
+	private int cod;
+	
+	private OrderStatus(int cod) {
+		this.cod = cod;
+	}
+	
+	public int getCod() {
+		return cod;
+	}
+	
+	public static OrderStatus valueOf(int cod) {
+		for (OrderStatus value : OrderStatus.values()) {
+			if (value.getCod() == cod) {
+				return value;
+			}
+		}
+		throw new IllegalArgumentException("ivalid OrderStatus");
+	}
+	
+}
